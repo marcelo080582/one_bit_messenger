7822477b8e6c1bcf335fda155d02be503593c50f
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-02-13 14:26:52 -0300

    Added send chat feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :contacts, only: [:index, :new, :create, :destroy]
-  resources :chats, only: [:index, :show]
+  resources :chats, only: [:index, :show] do
+    resources :messages, only: :create, defaults: { format: :js }
+  end
 end
