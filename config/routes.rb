c1e3d837eb805a25b24349ec3d1be30cf42000b1
Marcelo Martins <marcelo.martins@cilia.com.br>
2020-02-19 13:51:31 -0300

    added feature to list chats and changed home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "chats#index"
 
   resources :contacts, only: [:index, :new, :create, :destroy]
   resources :chats, only: [:index, :show] do
